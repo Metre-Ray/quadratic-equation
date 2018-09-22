e5c9b35aa766c9857c1c7babfb434dc2e37f6656
Metre-Ray <dgary@tut.by>
2018-09-22 14:15:36 +0300

    modified index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var i = equation.indexOf('x^2'),
+      j = equation.lastIndexOf('x');
+  var a = parseInt(equation.slice(0, i - 3));
+  var b = parseInt(equation[i + 4] + equation.slice(i + 6, j - 3));
+  var c = parseInt(equation[j + 2] + equation.slice(j + 4));
+  var d = b**2 - 4 * a * c;
+  var x1 = Math.round((-b + d**(1/2)) / (2 * a));
+  var x2 = Math.round((-b - d**(1/2)) / (2 * a));
+  if (x1 > x2) return [x2, x1];
+  return [x1, x2];
 }
